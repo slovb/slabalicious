7d08e5a698fcab66e9018663c1eaa5d965a6e506
Rickard Nyman <nyman.rickard@gmail.com>
2020-09-28 18:11:39 +0200

    Overlays and bottoms added

--- src/main/java/com/slov/slabalicious/objects/blocks/GrassSlab.java
+++ src/main/java/com/slov/slabalicious/objects/blocks/GrassSlab.java
@@ -5,19 +5,22 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.SlabBlock;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.SlabType;
+import net.minecraft.util.BlockRenderLayer;
 
 public class GrassSlab extends SlabBlock {
-	
+
 	public GrassSlab(Properties properties) {
 		super(properties);
-		this.setDefaultState(this.getDefaultState()
-				.with(TYPE, SlabType.BOTTOM)
-				.with(WATERLOGGED, Boolean.valueOf(false))
-		);
+		this.setDefaultState(
+				this.getDefaultState().with(TYPE, SlabType.BOTTOM).with(WATERLOGGED, Boolean.valueOf(false)));
 	}
 
 	@Override
 	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
 		builder.add(TYPE, WATERLOGGED);
+	}
+
+	public BlockRenderLayer getRenderLayer() {
+		return BlockRenderLayer.CUTOUT_MIPPED;
 	}
 }
